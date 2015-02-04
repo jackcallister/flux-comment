6f61c80296c2078e25dfd083e6dfea7bf14c6f8a
jarsbe <jarsbe@gmail.com>
2015-02-04 23:19:04 +1300

    Switch to call app.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var App = require('./components/app.react');
+var React = require('react');
+
+window.React = React; // export for http://fb.me/react-devtools
+
+React.render(<App />, document.getElementById('app'));
